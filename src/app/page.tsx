bb072e9d8457edc2352dff40e56f19a482087469
victorzarzar <victorzarzar58@gmail.com>
2024-05-18 12:38:56 -0300

    Feat: create Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./components/Header/Header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-   
+      <Header />
     </main>
   );
 }
